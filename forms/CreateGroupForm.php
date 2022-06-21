c9efaf9a52706df394159436a71e8f42c8f8bf91
bouledepate <promtheus815@gmail.com>
2022-06-21 21:38:37 +0600

    Application update 11.2 Fix

--- forms/CreateGroupForm.php
+++ forms/CreateGroupForm.php
@@ -8,14 +8,13 @@ use yii\base\Model;
 class CreateGroupForm extends Model
 {
     public $name;
-    public $module;
     public $speciality;
 
     public function rules()
     {
         return [
-            'require' => [['name', 'module', 'speciality'], 'required'],
-            'length' => [['name', 'module', 'speciality'], 'string', 'max' => 255]
+            'require' => [['name', 'speciality'], 'required'],
+            'length' => [['name', 'speciality'], 'string', 'max' => 255]
         ];
     }
 }
